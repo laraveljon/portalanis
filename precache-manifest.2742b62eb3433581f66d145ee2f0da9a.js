0aa37a33a52e0dd98be567ce9b2737aa5aea39ed
laraveljon <jar_alanis@outlook.com>
2020-06-23 15:06:05 -0500

    Updates

---
+++ precache-manifest.2742b62eb3433581f66d145ee2f0da9a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "107c0af969765e96dfdb",
+    "url": "/portalanis/static/css/main.a7389f04.chunk.css"
+  },
+  {
+    "revision": "107c0af969765e96dfdb",
+    "url": "/portalanis/static/js/main.107c0af9.chunk.js"
+  },
+  {
+    "revision": "b14594404b9331031b87",
+    "url": "/portalanis/static/js/1.b1459440.chunk.js"
+  },
+  {
+    "revision": "8cc48a7a45f6a2a4d373",
+    "url": "/portalanis/static/js/runtime~main.8cc48a7a.js"
+  },
+  {
+    "revision": "e5e92c5ae676059277b891356fb2cf49",
+    "url": "/portalanis/static/media/CapturaEdit.e5e92c5a.jpg"
+  },
+  {
+    "revision": "5e52babcf734f7ae4ddbaafc390cf53a",
+    "url": "/portalanis/static/media/cotizador.5e52babc.PNG"
+  },
+  {
+    "revision": "473d9f55d2f5045e41e3271c338cdb11",
+    "url": "/portalanis/static/media/project2.473d9f55.jpg"
+  },
+  {
+    "revision": "139308d797d4a548920ce783be94237f",
+    "url": "/portalanis/static/media/project3.139308d7.jpg"
+  },
+  {
+    "revision": "5a100b46aa5dcadfabc907a76cf78c87",
+    "url": "/portalanis/static/media/project4.5a100b46.jpg"
+  },
+  {
+    "revision": "babc9580b3b97699fb8a21b526912d87",
+    "url": "/portalanis/static/media/project5.babc9580.jpg"
+  },
+  {
+    "revision": "7232c64e540f324c24f96dd9e62f987c",
+    "url": "/portalanis/static/media/Captura2Edit.7232c64e.jpg"
+  },
+  {
+    "revision": "729df6fb907f554e9d22b617f960ebc5",
+    "url": "/portalanis/static/media/Captura2.729df6fb.PNG"
+  },
+  {
+    "revision": "a18322efe2eaf34931a079b9fbbc45cf",
+    "url": "/portalanis/static/media/Emili_perron4.a18322ef.gif"
+  },
+  {
+    "revision": "88ef23021887202a1d5ab4880d2d9930",
+    "url": "/portalanis/index.html"
+  }
+];
